5b398a5cf3d7f8de20ef81687b12369a275a231a
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-06-07 15:36:54 -0700

    update

---
+++ _notes/mongo.md
@@ -0,0 +1,4 @@
+
+ 
+$ npm install --save mongoose
+var mongoose require('mongoose');
---
+++ models/entity.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var schema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+
+
+});
+module.exports = mongoose.model('Message', schema);
